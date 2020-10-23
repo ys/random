5765104b921220f2043aceacdc81b93927c9524f
Yannick Schutz <yschutz@salesforce.com>
2020-10-23 17:33:44 +0200

    Stop removing it

--- themes/term/postcss.config.js
+++ themes/term/postcss.config.js
@@ -7,7 +7,8 @@ module.exports = {
         themeDir + "../../public/**/*.html"
       ],
       whitelistPatternsChildren: [
-       /mode-dark/
+       /mode-dark/,
+       /content/
       ]
     }),
     require("tailwindcss")(themeDir + "tailwind.config.js"),
